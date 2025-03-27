404383f32b420f44e837dd8d6628cb773b4fbf35
Socrates Alexiou <sochaxfresh@gmail.com>
2025-03-27 18:21:03 +0200

    Updated Supplier stuff

--- SupplierForm.Designer.cs
+++ SupplierForm.Designer.cs
@@ -189,7 +189,7 @@
             this.groupBox1.Controls.Add(this.checkBoxPreferred);
             this.groupBox1.Controls.Add(this.labelActive);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(204, 34);
+            this.groupBox1.Location = new System.Drawing.Point(204, 35);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(376, 340);
             this.groupBox1.TabIndex = 12;

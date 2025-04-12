a0d2edaf1bbc8fa4fb2d1be4202c20a5deb4f83b
Socrates Alexiou <sochaxfresh@gmail.com>
2025-04-13 02:45:16 +0300

    updated second reorderform

--- OrderLowStockForm.Designer.cs
+++ OrderLowStockForm.Designer.cs
@@ -30,38 +30,79 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridViewInvoiceItems = new System.Windows.Forms.DataGridView();
+            this.buttonConfirmOrder = new System.Windows.Forms.Button();
+            this.labelTotalCost = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInvoiceItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 404);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 569);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.Size = new System.Drawing.Size(99, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Back";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridViewInvoiceItems
             // 
             this.dataGridViewInvoiceItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewInvoiceItems.Location = new System.Drawing.Point(407, 12);
+            this.dataGridViewInvoiceItems.Location = new System.Drawing.Point(201, 55);
             this.dataGridViewInvoiceItems.Name = "dataGridViewInvoiceItems";
-            this.dataGridViewInvoiceItems.Size = new System.Drawing.Size(604, 375);
+            this.dataGridViewInvoiceItems.Size = new System.Drawing.Size(726, 370);
             this.dataGridViewInvoiceItems.TabIndex = 1;
+            // 
+            // buttonConfirmOrder
+            // 
+            this.buttonConfirmOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConfirmOrder.Location = new System.Drawing.Point(972, 569);
+            this.buttonConfirmOrder.Name = "buttonConfirmOrder";
+            this.buttonConfirmOrder.Size = new System.Drawing.Size(131, 35);
+            this.buttonConfirmOrder.TabIndex = 2;
+            this.buttonConfirmOrder.Text = "Confirm Order";
+            this.buttonConfirmOrder.UseVisualStyleBackColor = true;
+            this.buttonConfirmOrder.Click += new System.EventHandler(this.buttonConfirmOrder_Click);
+            // 
+            // labelTotalCost
+            // 
+            this.labelTotalCost.AutoSize = true;
+            this.labelTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalCost.Location = new System.Drawing.Point(450, 442);
+            this.labelTotalCost.Name = "labelTotalCost";
+            this.labelTotalCost.Size = new System.Drawing.Size(70, 25);
+            this.labelTotalCost.TabIndex = 3;
+            this.labelTotalCost.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SokProodos.Properties.Resources.pngwing_com__18_;
+            this.pictureBox1.Location = new System.Drawing.Point(-43, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1161, 625);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // OrderLowStockForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 475);
+            this.ClientSize = new System.Drawing.Size(1115, 615);
+            this.Controls.Add(this.labelTotalCost);
+            this.Controls.Add(this.buttonConfirmOrder);
             this.Controls.Add(this.dataGridViewInvoiceItems);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "OrderLowStockForm";
             this.Text = "OrderLowStockForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInvoiceItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +110,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridViewInvoiceItems;
+        private System.Windows.Forms.Button buttonConfirmOrder;
+        private System.Windows.Forms.Label labelTotalCost;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- OrderLowStockForm.cs
+++ OrderLowStockForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,17 @@ namespace SokProodos
     public partial class OrderLowStockForm: Form
     {
         private List<DataRow> selectedProducts;
+        private DataTable invoiceTable;
+        private string connectionString = @"Server=SOCHAX\SQLEXPRESS;Database=AdventureWorks2022;Trusted_Connection=True;";
         public OrderLowStockForm(List<DataRow> selectedProducts)
         {
             InitializeComponent();
             this.selectedProducts = selectedProducts;
             this.StartPosition = FormStartPosition.CenterScreen;
             LoadSelectedProducts();
+            UIStyler.StyleButtonsInForm(this);
+            GroupBoxStyler.StyleGroupBoxesInForm(this);
+            StyleInvoiceGrid();
         }
         private void LoadSelectedProducts()
         {
@@ -27,22 +33,81 @@ namespace SokProodos
             dt.Columns.Add("Name", typeof(string));
             dt.Columns.Add("Quantity", typeof(int));
             dt.Columns.Add("QuantityToReorder", typeof(int));
+            dt.Columns.Add("UnitPrice", typeof(string)); // Use string to allow "N/A"
+            dt.Columns.Add("TotalPrice", typeof(string)); // Same for total
+
+            decimal totalCost = 0;
 
             foreach (DataRow row in selectedProducts)
             {
-                dt.Rows.Add(
-                    row["ProductID"],
-                    row["Name"],
-                    row["Quantity"],
-                    row["QuantityToReorder"]
-                );
+                int productId = Convert.ToInt32(row["ProductID"]);
+                string name = row["Name"].ToString();
+                int quantity = Convert.ToInt32(row["Quantity"]);
+                int quantityToReorder = Convert.ToInt32(row["QuantityToReorder"]);
+                decimal unitPrice = 0;
+                decimal totalPrice = 0;
+
+                // Get UnitPrice from DB
+                using (SqlConnection conn = new SqlConnection(@"Server=SOCHAX\SQLEXPRESS;Database=AdventureWorks2022;Trusted_Connection=True;"))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT ListPrice FROM Production.Product WHERE ProductID = @ProductID", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@ProductID", productId);
+                        object result = cmd.ExecuteScalar();
+                        if (result != DBNull.Value && Convert.ToDecimal(result) > 0)
+                        {
+                            unitPrice = Convert.ToDecimal(result);
+                            totalPrice = unitPrice * quantityToReorder;
+                            totalCost += totalPrice;
+
+                            dt.Rows.Add(productId, name, quantity, quantityToReorder,
+                                        unitPrice.ToString("C2"), totalPrice.ToString("C2"));
+                        }
+                        else
+                        {
+                            dt.Rows.Add(productId, name, quantity, quantityToReorder,
+                                        "N/A", "N/A");
+                        }
+                    }
+                }
             }
 
             dataGridViewInvoiceItems.DataSource = dt;
+
+            // Allow QuantityToReorder to be editable
+            foreach (DataGridViewColumn col in dataGridViewInvoiceItems.Columns)
+            {
+                col.ReadOnly = true;
+            }
+
+            if (dataGridViewInvoiceItems.Columns.Contains("QuantityToReorder"))
+            {
+                dataGridViewInvoiceItems.Columns["QuantityToReorder"].ReadOnly = false;
+            }
+
+            // Update total cost label
+            labelTotalCost.Text = $"Total Cost: {totalCost.ToString("C2")}";
         }
 
+        private void dataGridViewInvoiceItems_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dataGridViewInvoiceItems.Columns[e.ColumnIndex].Name == "QuantityToReorder")
+            {
+                var row = invoiceTable.Rows[e.RowIndex];
+                int qty = Convert.ToInt32(row["QuantityToReorder"]);
+                decimal unitPrice = Convert.ToDecimal(row["UnitPrice"]);
+                row["TotalPrice"] = qty * unitPrice;
 
-        
+                UpdateTotalCostLabel();
+            }
+        }
+
+        private void UpdateTotalCostLabel()
+        {
+            decimal total = invoiceTable.AsEnumerable().Sum(r => r.Field<decimal>("TotalPrice"));
+            labelTotalCost.Text = $"Total Cost: €{total:F2}";
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -51,5 +116,94 @@ namespace SokProodos
 
             this.Hide();
         }
+
+        private void StyleInvoiceGrid()
+        {
+            var grid = dataGridViewInvoiceItems;
+
+            // General UI
+            grid.EnableHeadersVisualStyles = false;
+            grid.BorderStyle = BorderStyle.None;
+            grid.RowHeadersVisible = false;
+
+            // Header Style
+            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(245, 245, 245);
+            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.FromArgb(40, 40, 40);
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            grid.ColumnHeadersHeight = 35;
+
+            // Cell Style
+            grid.DefaultCellStyle.BackColor = Color.White;
+            grid.DefaultCellStyle.ForeColor = Color.Black;
+            grid.DefaultCellStyle.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+            grid.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            grid.DefaultCellStyle.Padding = new Padding(4, 2, 4, 2);
+
+            // Alternating Rows
+            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 248, 255);
+
+            // Borders
+            grid.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            grid.GridColor = Color.Gainsboro;
+
+            // Columns
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.AutoResizeColumns();
+            grid.ScrollBars = ScrollBars.Both;
+
+            // Hover Effect
+            grid.CellMouseEnter += (s, e) =>
+            {
+                if (e.RowIndex >= 0)
+                    grid.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.FromArgb(235, 240, 255);
+            };
+
+            grid.CellMouseLeave += (s, e) =>
+            {
+                if (e.RowIndex >= 0)
+                {
+                    Color altBack = e.RowIndex % 2 == 0
+                        ? Color.White
+                        : Color.FromArgb(245, 248, 255);
+                    grid.Rows[e.RowIndex].DefaultCellStyle.BackColor = altBack;
+                }
+            };
+        }
+
+
+        private void buttonConfirmOrder_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Create SalesOrderHeader
+                SqlCommand cmdHeader = new SqlCommand(@"
+                    INSERT INTO Sales.SalesOrderHeader (OrderDate, DueDate, ShipDate, Status, OnlineOrderFlag, TotalDue)
+                    OUTPUT INSERTED.SalesOrderID
+                    VALUES (GETDATE(), GETDATE(), GETDATE(), 1, 1, 0)", conn);
+
+                int orderId = (int)cmdHeader.ExecuteScalar();
+
+                // Create SalesOrderDetail lines
+                foreach (DataRow row in invoiceTable.Rows)
+                {
+                    SqlCommand cmdDetail = new SqlCommand(@"
+                        INSERT INTO Sales.SalesOrderDetail (SalesOrderID, ProductID, OrderQty, UnitPrice)
+                        VALUES (@OrderID, @ProductID, @OrderQty, @UnitPrice)", conn);
+
+                    cmdDetail.Parameters.AddWithValue("@OrderID", orderId);
+                    cmdDetail.Parameters.AddWithValue("@ProductID", row["ProductID"]);
+                    cmdDetail.Parameters.AddWithValue("@OrderQty", row["QuantityToReorder"]);
+                    cmdDetail.Parameters.AddWithValue("@UnitPrice", row["UnitPrice"]);
+                    cmdDetail.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Order placed successfully!", "Confirmed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
     }
+    
 }
